04bf6982e8f0692996d370cbcdea5331a44b9f15
YusufLaidey <yusuflaideylee@gmail.com>
2022-10-08 09:50:57 +0100

    rendering of image fro headless cms

---
+++ src/pages/Gallery.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import { useStaticQuery, graphql } from 'gatsby'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+
+import styled from 'styled-components'
+
+const query = graphql`
+  query {
+    allFile(filter: { extension: { ne: "svg" } }) {
+      nodes {
+       name
+       childImageSharp {
+        gatsbyImageData(
+          layout: FIXED
+          placeholder: BLURRED
+          width: 200
+          height: 200
+        )
+       }
+      }
+    }
+  }
+`
+
+const Gallery = () => {
+    const data = useStaticQuery(query);
+    const nodes = data.allFile.nodes
+  return (
+    <Layout>
+    <Wrapper>
+      {nodes.map((image, index) => {
+        const {name} = image
+        const pathToImage = getImage(image)
+        return <article key = {index} className='item' >
+        <GatsbyImage image = {pathToImage} alt={name} className='gallery-img' />
+          <p>{name}</p>
+        </article>
+      } )}
+    </Wrapper>
+    </Layout>
+  )
+}
+
+const Wrapper = styled.section`
+display: flex;
+flex-wrap: wrap;
+.item {
+  margin-right: 1rem;
+}
+.gallery-img {
+  border-radius: 1rem;
+}
+`
+
+export default Gallery
