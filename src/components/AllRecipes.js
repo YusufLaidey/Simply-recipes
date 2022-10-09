9f0ff8d184c845485ca21b74c121973fff5ee650
YusufLaidey <yusuflaideylee@gmail.com>
2022-10-09 10:28:06 +0100

    rendering the image from a backend

--- src/components/AllRecipes.js
+++ src/components/AllRecipes.js
@@ -1,17 +1,40 @@
 import React from 'react'
 
+import { graphql, useStaticQuery } from 'gatsby'
 
 import TagsList from './TagsList'
 import RecipesList from './RecipesList'
 
+const query = graphql`
+ {
+  allContentfulRecipe(sort: { fields: title, order: ASC }) {
+    nodes {
+      id
+      title
+      cookTime
+      prepTime
+      content {
+        tags
+      }
+      image {
+        gatsbyImageData(layout: CONSTRAINED, placeholder: TRACED_SVG)
+      }
+    }
+  }
+}
+`
 
 const AllRecipes = () => {
+  const data = useStaticQuery(query)
+  // const {allContentfulRecipe:{nodes:recipes}} = useStaticQuery(query)
+  const recipes = data.allContentfulRecipe.nodes
+  console.log(recipes)
   return (
-    <div>
+    <section className='recipes-container'>
       <h4>all recipes</h4>
-      <RecipesList/>
-      <TagsList/>
-    </div>
+      <RecipesList recipes={recipes} />
+      <TagsList recipes={recipes} />
+    </section>
   )
 }
 
--- src/components/RecipesList.js
+++ src/components/RecipesList.js
@@ -1,11 +1,21 @@
 import React from 'react'
 
-const RecipesList = () => {
-  return (
-    <div>
-      <h5>recipes list</h5>
-    </div>
-  )
+import { Link } from 'gatsby'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+
+const RecipesList = ({ recipes = [] }) => {
+  return <div className='recipes-list'>
+    {recipes.map((recipe) =>{
+      const { id, title, image, prepTime, cookTime} = recipe
+      const pathToImage = getImage(image)
+      return <Link key={id} to={`/${title}`} className='recipe'>
+        <GatsbyImage image={pathToImage} className='recipe-img' alt={title} />
+        <h4>{title}</h4>
+        <p>Prep:{prepTime}min | Cook:{cookTime}min</p>
+      </Link>
+    }
+  )}
+  </div>
 }
 
 export default RecipesList
