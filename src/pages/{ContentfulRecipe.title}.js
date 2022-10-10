a6f29f87b6651350820758b6867549bcb3fd5120
YusufLaidey <yusuflaideylee@gmail.com>
2022-10-10 23:57:45 +0100

    rendered the contents of the recipes pages

--- src/pages/{ContentfulRecipe.title}.js
+++ src/pages/{ContentfulRecipe.title}.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-import { graphql } from 'gatsby'
+import Layout from '../components/Layout'
+import { graphql, Link } from 'gatsby'
 import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 
-import { BsClockHistory, BsClock, Bspeople } from 'react-icons/bs'
+import { BsClockHistory, BsClock, Bspeople, BsPeople } from 'react-icons/bs'
 
 const RecipeTemplate = ({ data }) => {
     const {
@@ -17,11 +18,80 @@ const RecipeTemplate = ({ data }) => {
       } = data.contentfulRecipe
     const pathToImage = getImage(image)
     const { tags, instructions, ingredients, tools } = content
-  return <main className='page'>
-    <div className='recipe-page'>
-        <h2>{title}</h2>
-    </div>
-  </main>
+    
+  return (
+    <Layout>
+      <main className='page'>
+        <div className='recipe-page'>
+          {/* hero */}
+          <section className='recipe-hero'>
+            <GatsbyImage image={pathToImage} alt={title} className='about-img' />
+            <article className='recipe-info'>
+              <h2>{title}</h2>
+              <p>{description}</p>
+              {/* icons */}
+              <div className='recipe-icons'>
+              <article>
+                  <BsClockHistory />
+                  <h5>prepTime</h5>
+                  <p>{prepTime} mins</p>
+                </article>
+                <article>
+                  <BsClock />
+                  <h5>cookTime</h5>
+                  <p>{cookTime} mins</p>
+                </article>
+                <article>
+                  <BsPeople />
+                  <h5>servings</h5>
+                  <p>{servings} plates</p>
+                </article>
+              </div>
+              {/* tags */}
+              <p className='recipe-tags'>
+                Tags: {tags.map((tag, index) => {
+                  return <Link to={`/${tag}`} key={index}>{tag}</Link>
+                })}
+              </p>
+            </article>
+          </section>
+          {/* rest of the content */}
+          <section className='recipe-content'>
+            <article>
+              <h4>instructions</h4>
+              {instructions.map((item, index) => {
+                return <div key={index} className='single-instruction'>
+                  <header>
+                    <p>step {index + 1}</p>
+                    <div></div>
+                  </header>
+                  <p>{item}</p>
+                </div>
+              })}
+            </article>
+            <article className='second-column'>
+              <div>
+                <h4>ingredients</h4>
+                {ingredients.map((item, index) => {
+                  return <p key={index} className='single-ingredient'>
+                    {item}
+                  </p>
+                })}
+              </div>
+              <div>
+                <h4>tools</h4>
+                {tools.map((item, index) => {
+                  return <p key={index} className='single-tool'>
+                    {item}
+                  </p>
+                })}
+              </div>
+            </article>
+          </section>
+        </div>
+      </main>
+    </Layout>
+  )
 }
 
 export const query =  graphql`
