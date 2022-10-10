efdd2c8ccb757f50fd8870303d560ade10a78307
YusufLaidey <yusuflaideylee@gmail.com>
2022-10-10 22:03:32 +0100

    rendering of tags page

--- src/components/RecipesList.js
+++ src/components/RecipesList.js
@@ -2,13 +2,15 @@ import React from 'react'
 
 import { Link } from 'gatsby'
 import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+import slugify from 'slugify'
 
 const RecipesList = ({ recipes = [] }) => {
   return <div className='recipes-list'>
     {recipes.map((recipe) =>{
       const { id, title, image, prepTime, cookTime} = recipe
       const pathToImage = getImage(image)
-      return <Link key={id} to={`/${title}`} className='recipe'>
+      const slug = slugify(title, { lower: true})
+      return <Link key={id} to={`/${slug}`} className='recipe'>
         <GatsbyImage image={pathToImage} className='recipe-img' alt={title} />
         <h4>{title}</h4>
         <p>Prep:{prepTime}min | Cook:{cookTime}min</p>
--- src/pages/{ContentfulRecipe.title}.js
+++ src/pages/{ContentfulRecipe.title}.js
@@ -1,12 +1,50 @@
 import React from 'react'
 
-const RecipeTemplate = props => {
+import { graphql } from 'gatsby'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 
-  return (
-    <div>
-      <h2>{props.params.title}</h2>
+import { BsClockHistory, BsClock, Bspeople } from 'react-icons/bs'
+
+const RecipeTemplate = ({ data }) => {
+    const {
+        title,
+        cookTime,
+        content,
+        prepTime,
+        servings,
+        description: { description },
+        image,
+      } = data.contentfulRecipe
+    const pathToImage = getImage(image)
+    const { tags, instructions, ingredients, tools } = content
+  return <main className='page'>
+    <div className='recipe-page'>
+        <h2>{title}</h2>
     </div>
-  )
+  </main>
 }
+
+export const query =  graphql`
+  query getSingleRecipe($title: String) {
+    contentfulRecipe(title: { eq: $title}) {
+        title
+        cookTime
+        content {
+            ingredients
+            instructions
+            tags
+            tools
+        }
+        description {
+            description
+        }
+        prepTime
+        servings
+        image {
+            gatsbyImageData(layout: CONSTRAINED, placeholder: TRACED_SVG)
+        }
+    }
+  }
+`
 
 export default RecipeTemplate
