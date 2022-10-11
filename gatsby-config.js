6f8607f3d8444e3b759fb30a7df39a3a4130b3fa
YusufLaidey <yusuflaideylee@gmail.com>
2022-10-11 09:56:32 +0100

    fonts preloading

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,5 +41,22 @@ module.exports = {
     },
   },
   `gatsby-plugin-image`,
+  {
+    resolve: `gatsby-plugin-webfonts`,
+    options: {
+      fonts: {
+        google: [
+          {
+            family: "Montserrat",
+            variants: ["400"],
+          },
+          {
+            family: "Inconsolata",
+            variants: ["400", "500", "600", "700"],
+          },
+        ],
+      },
+    },
+  },
 ],
 }
